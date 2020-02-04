be10d0f0e7465cef4f90cd66c8a8d9ca03585f5f
Brad Findell <findell.2@osu.edu>
2020-02-04 14:18:24 -0500

    Mostly fixed through Level 12.
    The most common issue was that the wrong tools were displaying.

--- _includes/parameters480p.js
+++ _includes/parameters480p.js
@@ -2,7 +2,7 @@
     /** is3D=is 3D applet using 3D view, AV=Algebra View, SV=Spreadsheet View, CV=CAS View, EV2=Graphics View 2, CP=Construction Protocol, PC=Probability Calculator, DA=Data Analysis, FI=Function Inspector, PV=Python, macro=Macro View */
     var views = {"is3D":false,"AV":false,"SV":false,"CV":false,"EV2":false,"CP":false,"PC":false,"DA":false,"FI":false,"PV":false,"macro":true};
 	var applet = new GGBApplet('5.0', parameters, views);
-    applet.setPreviewImage('', '/img/GeoGebra_loading.png');
+    // applet.setPreviewImage('', '/img/GeoGebra_loading.png'); // comment out because image was squashed
     window.onload = function() {
         applet.inject('applet_container', 'html5');
     }
